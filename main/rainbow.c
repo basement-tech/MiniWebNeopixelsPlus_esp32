a512b623f98a4256d4ecf7e05fa77524012cc52d
djzman078 <djzman078@gmail.com>
2025-06-25 20:40:35 -0500

    starting rainbow port from Adafruit C++

---
+++ main/rainbow.c
@@ -0,0 +1,204 @@
+#ifdef PORTING_RAINBOW
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+uint16_t numLEDs = 20;
+uint8_t brightness = 255;
+
+/*!
+  @brief   Change the pixel format of a previously-declared
+           Adafruit_NeoPixel strip object. If format changes from one of
+           the RGB variants to an RGBW variant (or RGBW to RGB), the old
+           data will be deallocated and new data is cleared. Otherwise,
+           the old data will remain in RAM and is not reordered to the
+           new format, so it's advisable to follow up with clear().
+  @param   t  Pixel type -- add together NEO_* constants defined in
+              Adafruit_NeoPixel.h, for example NEO_GRB+NEO_KHZ800 for
+              NeoPixels expecting an 800 KHz (vs 400 KHz) data stream
+              with color bytes expressed in green, red, blue order per
+              pixel.
+  @note    This function is deprecated, here only for old projects that
+           may still be calling it. New projects should instead use the
+           'new' keyword with the first constructor syntax
+           (length, pin, type).
+*/
+typedef uint16_t neoPixelType; ///< 3rd arg to Adafruit_NeoPixel constructor
+neoPixelType wOffset, rOffset, gOffset, bOffset;
+void Adafruit_NeoPixel_updateType(neoPixelType t) {
+  bool oldThreeBytesPerPixel = (wOffset == rOffset); // false if RGBW
+
+  wOffset = (t >> 6) & 0b11; // See notes in header file
+  rOffset = (t >> 4) & 0b11; // regarding R/G/B/W offsets
+  gOffset = (t >> 2) & 0b11;
+  bOffset = t & 0b11;
+#if defined(NEO_KHZ400)
+  is800KHz = (t < 256); // 400 KHz flag is 1<<8
+#endif
+
+  // If bytes-per-pixel has changed (and pixel data was previously
+  // allocated), re-allocate to new size. Will clear any data.
+  if (pixels) {
+    bool newThreeBytesPerPixel = (wOffset == rOffset);
+    if (newThreeBytesPerPixel != oldThreeBytesPerPixel)
+      updateLength(numLEDs);
+  }
+}
+
+/*!
+  @brief   Set a pixel's color using a 32-bit 'packed' RGB or RGBW value.
+  @param   n  Pixel index, starting from 0.
+  @param   c  32-bit color value. Most significant byte is white (for RGBW
+              pixels) or ignored (for RGB pixels), next is red, then green,
+              and least significant byte is blue.
+*/
+void Adafruit_NeoPixel_setPixelColor(uint16_t n, uint32_t c) {
+  if (n < numLEDs) {
+    uint8_t *p, r = (uint8_t)(c >> 16), g = (uint8_t)(c >> 8), b = (uint8_t)c;
+    if (brightness) { // See notes in setBrightness()
+      r = (r * brightness) >> 8;
+      g = (g * brightness) >> 8;
+      b = (b * brightness) >> 8;
+    }
+    if (wOffset == rOffset) {
+      p = &pixels[n * 3];
+    } else {
+      p = &pixels[n * 4];
+      uint8_t w = (uint8_t)(c >> 24);
+      p[wOffset] = brightness ? ((w * brightness) >> 8) : w;
+    }
+    p[rOffset] = r;
+    p[gOffset] = g;
+    p[bOffset] = b;
+  }
+}
+
+/*!
+  @brief   Convert hue, saturation and value into a packed 32-bit RGB color
+           that can be passed to setPixelColor() or other RGB-compatible
+           functions.
+  @param   hue  An unsigned 16-bit value, 0 to 65535, representing one full
+                loop of the color wheel, which allows 16-bit hues to "roll
+                over" while still doing the expected thing (and allowing
+                more precision than the wheel() function that was common to
+                prior NeoPixel examples).
+  @param   sat  Saturation, 8-bit value, 0 (min or pure grayscale) to 255
+                (max or pure hue). Default of 255 if unspecified.
+  @param   val  Value (brightness), 8-bit value, 0 (min / black / off) to
+                255 (max or full brightness). Default of 255 if unspecified.
+  @return  Packed 32-bit RGB with the most significant byte set to 0 -- the
+           white element of WRGB pixels is NOT utilized. Result is linearly
+           but not perceptually correct, so you may want to pass the result
+           through the gamma32() function (or your own gamma-correction
+           operation) else colors may appear washed out. This is not done
+           automatically by this function because coders may desire a more
+           refined gamma-correction function than the simplified
+           one-size-fits-all operation of gamma32(). Diffusing the LEDs also
+           really seems to help when using low-saturation colors.
+*/
+uint32_t Adafruit_ColorHSV(uint16_t hue, uint8_t sat, uint8_t val) {
+
+  uint8_t r, g, b;
+
+  // Remap 0-65535 to 0-1529. Pure red is CENTERED on the 64K rollover;
+  // 0 is not the start of pure red, but the midpoint...a few values above
+  // zero and a few below 65536 all yield pure red (similarly, 32768 is the
+  // midpoint, not start, of pure cyan). The 8-bit RGB hexcone (256 values
+  // each for red, green, blue) really only allows for 1530 distinct hues
+  // (not 1536, more on that below), but the full unsigned 16-bit type was
+  // chosen for hue so that one's code can easily handle a contiguous color
+  // wheel by allowing hue to roll over in either direction.
+  hue = (hue * 1530L + 32768) / 65536;
+  // Because red is centered on the rollover point (the +32768 above,
+  // essentially a fixed-point +0.5), the above actually yields 0 to 1530,
+  // where 0 and 1530 would yield the same thing. Rather than apply a
+  // costly modulo operator, 1530 is handled as a special case below.
+
+  // So you'd think that the color "hexcone" (the thing that ramps from
+  // pure red, to pure yellow, to pure green and so forth back to red,
+  // yielding six slices), and with each color component having 256
+  // possible values (0-255), might have 1536 possible items (6*256),
+  // but in reality there's 1530. This is because the last element in
+  // each 256-element slice is equal to the first element of the next
+  // slice, and keeping those in there this would create small
+  // discontinuities in the color wheel. So the last element of each
+  // slice is dropped...we regard only elements 0-254, with item 255
+  // being picked up as element 0 of the next slice. Like this:
+  // Red to not-quite-pure-yellow is:        255,   0, 0 to 255, 254,   0
+  // Pure yellow to not-quite-pure-green is: 255, 255, 0 to   1, 255,   0
+  // Pure green to not-quite-pure-cyan is:     0, 255, 0 to   0, 255, 254
+  // and so forth. Hence, 1530 distinct hues (0 to 1529), and hence why
+  // the constants below are not the multiples of 256 you might expect.
+
+  // Convert hue to R,G,B (nested ifs faster than divide+mod+switch):
+  if (hue < 510) { // Red to Green-1
+    b = 0;
+    if (hue < 255) { //   Red to Yellow-1
+      r = 255;
+      g = hue;       //     g = 0 to 254
+    } else {         //   Yellow to Green-1
+      r = 510 - hue; //     r = 255 to 1
+      g = 255;
+    }
+  } else if (hue < 1020) { // Green to Blue-1
+    r = 0;
+    if (hue < 765) { //   Green to Cyan-1
+      g = 255;
+      b = hue - 510;  //     b = 0 to 254
+    } else {          //   Cyan to Blue-1
+      g = 1020 - hue; //     g = 255 to 1
+      b = 255;
+    }
+  } else if (hue < 1530) { // Blue to Red-1
+    g = 0;
+    if (hue < 1275) { //   Blue to Magenta-1
+      r = hue - 1020; //     r = 0 to 254
+      b = 255;
+    } else { //   Magenta to Red-1
+      r = 255;
+      b = 1530 - hue; //     b = 255 to 1
+    }
+  } else { // Last 0.5 Red (quicker than % operator)
+    r = 255;
+    g = b = 0;
+  }
+
+  // Apply saturation and value to R,G,B, pack into 32-bit result:
+  uint32_t v1 = 1 + val;  // 1 to 256; allows >>8 instead of /255
+  uint16_t s1 = 1 + sat;  // 1 to 256; same reason
+  uint8_t s2 = 255 - sat; // 255 to 0
+  return ((((((r * s1) >> 8) + s2) * v1) & 0xff00) << 8) |
+         (((((g * s1) >> 8) + s2) * v1) & 0xff00) |
+         (((((b * s1) >> 8) + s2) * v1) >> 8);
+}
+
+/*!
+  @brief   Fill NeoPixel strip with one or more cycles of hues.
+           Everyone loves the rainbow swirl so much, now it's canon!
+  @param   first_hue   Hue of first pixel, 0-65535, representing one full
+                       cycle of the color wheel. Each subsequent pixel will
+                       be offset to complete one or more cycles over the
+                       length of the strip.
+  @param   reps        Number of cycles of the color wheel over the length
+                       of the strip. Default is 1. Negative values can be
+                       used to reverse the hue order.
+  @param   saturation  Saturation (optional), 0-255 = gray to pure hue,
+                       default = 255.
+  @param   brightness  Brightness/value (optional), 0-255 = off to max,
+                       default = 255. This is distinct and in combination
+                       with any configured global strip brightness.
+  @param   gammify     If true (default), apply gamma correction to colors
+                       for better appearance.
+*/
+void Adafruit_NeoPixel_rainbow(uint16_t first_hue, int8_t reps,
+  uint8_t saturation, uint8_t brightness, bool gammify) {
+  for (uint16_t i=0; i<numLEDs; i++) {
+    uint16_t hue = first_hue + (i * reps * 65536) / numLEDs;
+    uint32_t color = Adafruit_ColorHSV(hue, saturation, brightness);
+//    if (gammify) color = gamma32(color);
+    Adafruit_NeoPixel_setPixelColor(i, color);
+  }
+}
+
+#endif
