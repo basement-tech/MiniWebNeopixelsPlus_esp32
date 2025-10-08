d2ab129dcf95653ec9ae1d06f8e87ee4c958c51c
djzman078 <djzman078@gmail.com>
2025-10-08 18:09:57 -0500

    continuing ....

--- main/neo_script.c
+++ main/neo_script.c
@@ -2,6 +2,7 @@
  * script engine
  */
 #include "stdint.h"
+#include "string.h"
 #include "esp_log.h"
 
 #include "neo_script.h"
@@ -15,29 +16,40 @@ static uint8_t script_state = NEO_SCRIPT_STOPPED;
  * see if the script needs to move to the next step
  */
 int8_t script_update(bool new_data)  {
+    char filename[MAX_FILENAME];
+    neo_script_cmd_t cmd_type = NEO_CMD_SCRIPT_UNDEFINED;
 
     switch(script_state)  {
 
-        case NEO_SCRIPT_STOPPED:
+        case NEO_SCRIPT_STOPPED:  // no current activity  ... waiting for signal to start
+            if(new_data == true)  {
+                if(xSemaphoreTake(xscriptMutex, 0) == pdTRUE)  {
+                    cmd_type = script_mutex_data.cmd_type;
+                    strncpy(filename, script_mutex_data.filename, sizeof(filename));
+                    new_data = false;
+                    xSemaphoreGive(xscriptMutex);
+                }
+                if(cmd_type != NEO_CMD_SCRIPT_START)
+                    ESP_LOGI(TAG, "invalid cmd_type received while stopped");
+                else  {
+                    ESP_LOGI(TAG, "starting new script from STOPPED");
+                    script_state = NEO_SCRIPT_START;
+                }
+            }
+            break;
+
+        case NEO_SCRIPT_STOPPING:  // cleaning up and stopping
         
             break;
 
-        case NEO_SCRIPT_STOPPING:
-        
-            break;
-
-        case NEO_SCRIPT_START:
+        case NEO_SCRIPT_START:  // initializing new script and sending first step
         
             break;
         
-        case NEO_SCRIPT_WAIT:
+        case NEO_SCRIPT_WAIT:  // waiting for signal that step has completed, send next step
         
             break;
         
-        case NEO_SCRIPT_WRITE:
-        
-            break;
-
         default:
             ESP_LOGD(TAG, "Invalid State");  //thread safety test
             break;
--- main/neo_script.h
+++ main/neo_script.h
@@ -13,12 +13,18 @@
 #define NEO_SCRIPT_WAIT     3
 #define NEO_SCRIPT_WRITE    4
 
-
+typedef enum  {
+    NEO_CMD_SCRIPT_START,      // start a new script please
+    NEO_CMD_SCRIPT_STEP_NEXT,  // next step (e.g. seq ended or other)
+    NEO_CMD_SCRIPT_STOP_REQ,   // stop the current script please
+    NEO_CMD_SCRIPT_UNDEFINED   // for init and error checking
+} neo_script_cmd_t;
 
 /*
  * script process startup and IPC communication constructs
  */
 typedef struct  {
+    neo_script_cmd_t cmd_type;  // what is the neo process trying to say
     char filename[MAX_FILENAME];
     bool new_data;   // true if new sequence request has been made
 } script_mutex_data_t;
